eb4f9eeec31bd3afb455a5ddd739b1005850d60d
zdenek82 <zdenek.vacek82@gmail.com>
2021-10-15 11:09:09 +0200

    added new scripts

--- Time.py
+++ Time.py
@@ -1,5 +1,5 @@
 time = input('Please enter your time: ')
 hours, mins = time.split(':')
-adjusted_hours = hours if int(hours)==12 else str(int(hours) % 12)
-daytime = ['AM','PM'][int(hours)>=12]
+adjusted_hours = hours if int(hours) == 12 else str(int(hours) % 12)
+daytime = ['AM', 'PM'][int(hours) >= 12]
 print('Converted to English format: ' + adjusted_hours + ':' + mins + ' ' + daytime)
